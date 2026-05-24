9b24c8121d27e72d28fd725b4b37c3944dd6fcbf
Thanaphon <voigkgky@gmail.com>
2026-05-24 13:36:10 +0700

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./pages/home/home').then(m => m.Home)
+    },
+    {
+        path: 'program',
+        loadComponent: () => import('./pages/program/program').then(m => m.Program)
+    },
+    {
+        path: 'contact',
+        loadComponent: () => import('./pages/contact/contact').then(m => m.Contact)
+    },
+     {
+    path: '**',
+    loadComponent: () =>
+      import('./pages/not-found/not-found')
+      .then(m => m.NotFound)
+  }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterOutlet, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('relume');
+
 }
